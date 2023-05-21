1120c1c56d0c15296668e54c14f3ebeeb781179c
极客小白_白 <49304819+Jikexiaobai@users.noreply.github.com>
2023-05-21 23:59:10 +0800

    修复语法错误

--- upload.py
+++ upload.py
@@ -193,7 +193,7 @@ def upload_video(video_file, cover_file, _config, detail):
         pushplus_data = {
         "token": "74dadec01cd345e5bb01204bef88fb97",
         "title": "搬运失败《" + detail['title'] + "》",
-        "content": "稿件《" + detail['title'] + "》" + "\n报错信息 " + raise Exception(p.stdout.read()) + "\n具体报错为:p.stdout.read()"
+        "content": "稿件《" + detail['title'] + "》" + "\n报错信息为 " + p.stdout.read()
         }
         res = requests.post("http://www.pushplus.plus/send", data=pushplus_data, proxies=PROXY)
     buf = p.stdout.read().splitlines(keepends=False)
